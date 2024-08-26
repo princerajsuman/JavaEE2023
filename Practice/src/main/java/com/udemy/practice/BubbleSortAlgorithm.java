1704ba2250ba181c90e100ba99b55cec9b90b023
Prince Raj Suman <princerajsuman@gmail.com>
2024-08-27 01:21:55 +0530

    New feature

--- Practice/src/main/java/com/udemy/practice/BubbleSortAlgorithm.java
+++ Practice/src/main/java/com/udemy/practice/BubbleSortAlgorithm.java
@@ -8,4 +8,18 @@ public class BubbleSortAlgorithm implements SortAlgorithm{
 //		sorting logic
 		return number;
 	}
+
+	public int[] sort(int[] number, int n) {
+//		sorting logic
+		return number;
+	}
+
+//	public String sort(int[] number) {
+////		sorting logic
+//		long[] arr = new long[number.length];
+//		for(int i = 0; i < number.length-1; i++){
+//			arr[i] = (long) number[i];
+//		}
+//		return "arr";
+//	}
 }
