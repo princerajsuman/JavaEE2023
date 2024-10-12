062062ffdbf97b273d9b19310355fd586a3051e3
Prince Raj Suman <princerajsuman@gmail.com>
2024-10-12 21:41:36 +0530

    This is second change

--- Practice/src/main/java/com/udemy/practice/QuickSortAlgorithm.java
+++ Practice/src/main/java/com/udemy/practice/QuickSortAlgorithm.java
@@ -3,6 +3,7 @@ package com.udemy.practice;
 public class QuickSortAlgorithm implements SortAlgorithm{
 	public int[] sort(int[] number) {
 //		sorting logic
+		// this is second change
 		return number;
 	}
 }
